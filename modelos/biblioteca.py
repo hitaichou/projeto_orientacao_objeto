914ebda4d9dbc986722ee7c79c4ffdc49da0274d
Raphael Fernandes <fernandes.raphael@live.com>
2025-11-19 07:00:29 -0300

    Commit métodos.

--- modelos/biblioteca.py
+++ modelos/biblioteca.py
@@ -9,9 +9,11 @@ class Biblioteca:
     def __str__(self):
         return self.nome
     
-    def listar_bibliotecas():
+    @classmethod #método de classe para listar todas as bibliotecas
+    def listar_bibliotecas(cls): #cls refere-se à própria classe sem necessidade de instanciar um objeto
+        print(f"{'Nome da Biblioteca'.ljust(25)} | {'Estado'}") #crio o cabeçalho da tabela
         for biblioteca in Biblioteca.bibliotecas:
-            print(f"{biblioteca.nome} | Ativo: {biblioteca.ativo}")
+            print(f"{biblioteca.nome.ljust(25)} | Ativo: {biblioteca.ativo}") #ljust alinha o texto à esquerda com 25 caracteres de largura
     
     #alterna o estado da biblioteca entre ativo e inativo
     #busca o atributo privado _ativo e inverte seu valor 
@@ -36,4 +38,4 @@ biblioteca_cidade.alterna_estado()  # Ativa a biblioteca
 biblioteca_shopping.alterna_estado()  # Ativa a biblioteca
 #print(biblioteca_shopping)  # Output: Biblioteca do Shopping
 
-Biblioteca.listar_bibliotecas()
+biblioteca_cidade.listar_bibliotecas()
