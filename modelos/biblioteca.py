d667ef2f8976a3f0f75660724e3d4b5f6ec74baa
Raphael Fernandes <fernandes.raphael@live.com>
2025-11-19 06:49:59 -0300

    Commit encapsulamento.

--- modelos/biblioteca.py
+++ modelos/biblioteca.py
@@ -2,7 +2,8 @@ class Biblioteca:
     bibliotecas = []
     def __init__(self, nome):
         self.nome = nome
-        self.ativo = "False"        
+        #inicialmente desativada, utilizo underscore para indicar atributo privado
+        self._ativo = False 
         Biblioteca.bibliotecas.append(self) #adiciona a biblioteca na lista de bibliotecas
     
     def __str__(self):
@@ -12,11 +13,27 @@ class Biblioteca:
         for biblioteca in Biblioteca.bibliotecas:
             print(f"{biblioteca.nome} | Ativo: {biblioteca.ativo}")
     
+    #alterna o estado da biblioteca entre ativo e inativo
+    #busca o atributo privado _ativo e inverte seu valor 
+    #podemos chamar este método de SET
+    def alterna_estado(self):        
+        self._ativo = not self._ativo
+    
+    #property para acessar o estado da biblioteca, sem expor o atributo privado diretamente
+    #neste caso, retorna "ativada" ou "desativada" baseado no valor booleano de _ativo
+    #Aqui é feito o GET do atributo
+    @property
+    def ativo(self):
+        return "ativada" if self._ativo else "desativada"
+    
 biblioteca_cidade = Biblioteca("Biblioteca da Cidade") 
+#biblioteca_cidade._ativo = "True"
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")  
 
  
-print(biblioteca_cidade)  # Output: Biblioteca da Cidade
-print(biblioteca_shopping)  # Output: Biblioteca do Shopping
+biblioteca_cidade.alterna_estado()  # Ativa a biblioteca
+#print(biblioteca_cidade)  # Output: Biblioteca da Cidade
+biblioteca_shopping.alterna_estado()  # Ativa a biblioteca
+#print(biblioteca_shopping)  # Output: Biblioteca do Shopping
 
 Biblioteca.listar_bibliotecas()
